17eab9a2c257dd07791256dc30755394edb651af
Toni <antoniotumbic@gmail.com>
2025-01-21 17:50:22 +0100

    Initial commit

---
+++ src/test/java/com/expression/evaluator/ExpressionEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expression.evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressionEvaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
