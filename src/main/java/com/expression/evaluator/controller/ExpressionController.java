53ef941bafc4236eae7e356fae80ed0903ea752f
Toni <antoniotumbic@gmail.com>
2025-01-22 17:27:30 +0100

    Add controllers path

--- src/main/java/com/expression/evaluator/controller/ExpressionController.java
+++ src/main/java/com/expression/evaluator/controller/ExpressionController.java
@@ -3,7 +3,7 @@ package com.expression.evaluator.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/expression")
+@RequestMapping(path = "/expression")
 public class ExpressionController {
 
     @PostMapping
